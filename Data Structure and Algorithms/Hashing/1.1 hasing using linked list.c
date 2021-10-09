a709bfd9e032302a1611e416c67cb89afcbfd820
Acfilok Kolifca <88615645+acfilok96@users.noreply.github.com>
2021-10-09 11:02:07 +0530

    Add files via upload

---
+++ Data Structure and Algorithms/Hashing/1.1 hasing using linked list.c
@@ -0,0 +1,71 @@
+// Hashing using Linked list
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}node;
+
+int key = 10 ;
+node* Hash_table[10];
+
+
+void Insert_element()
+{
+    int n;
+    printf("*****************************");
+    printf("\nenter element: ");
+    scanf("%d",&n);
+    int p=n%key;
+    node *tempo = Hash_table[p];
+    while(tempo->next!=NULL)
+    {
+        tempo=tempo->next;
+    }
+    node *temp=(node* )malloc(sizeof(node));
+    temp->data=n;
+    temp->next=NULL;
+    tempo->next=temp;
+    return;
+}
+
+
+void Show_table()
+{
+    printf("\nShow the Table\n");
+    for(int i=0;i<key;i++)
+    {
+        printf("%d --->\t",i);
+        node *tempo=Hash_table[i]->next;
+        while(tempo!=NULL)
+        {
+            printf("%d\t",tempo->data);
+            tempo = tempo->next;
+        }
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    for(int i=0;i<10;i++)
+    {
+        Hash_table[i]=(node *)malloc(sizeof(node));
+        Hash_table[i]->data=8766223;
+        Hash_table[i]->next=NULL;
+    }
+    while(1)
+    {
+        Insert_element();
+        Show_table();
+    }
+    return 0;
+}
+
